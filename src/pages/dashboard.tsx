38027b196601b474b0b444a5b752d79df0ac051a
puffer <devpuffer0807@gmail.com>
2024-11-22 21:50:28 +0900

    feat: add search option

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,20 @@
 
 import { Image, } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
 import Layout from "../layout";
 import { BgColor } from "../App";
 import BgImg from "../assets/img/bg.png";
 
 function Dashboard(props: any) {
-  const nav = useNavigate();
+
+  const searchHandler = async () => {
+
+  }
+
   return (
     <Layout>
       <div
@@ -17,7 +24,17 @@ function Dashboard(props: any) {
         <Image id="bg-img" src={BgImg} alt="background" />
 
         <div className="card-content justify-content-between px-lg-4 d-flex flex-column" style={{ minHeight: "70vh" }}>
-
+          <Card>
+            <Card.Body>
+              <Form.Group controlId="formFile" className="mt-3">
+                <Form.Label>Index of NFT</Form.Label>
+                <Stack direction="horizontal">
+                  <Form.Control type="text" onChange={() => { }} placeholder="Please input the index of NFT" style={{ maxWidth: 300 }} />
+                  <Button style={{ marginLeft: 10 }} onClick={searchHandler}>Search</Button>
+                </Stack>
+              </Form.Group>
+            </Card.Body>
+          </Card>
         </div>
       </div>
     </Layout>
